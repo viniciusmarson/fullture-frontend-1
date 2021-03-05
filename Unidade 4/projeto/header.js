57980b207813062f9cd5fa61dc2a1c56c80b0239
Vinicius Marson <vinicius.marson@singu.com.br>
2021-03-05 16:54:20 -0300

    fixed

--- Unidade 4/projeto/header.js
+++ Unidade 4/projeto/header.js
@@ -1,13 +1,13 @@
-$(window).on("scroll", function () {
-    if ($(window).scrollTop() > 50) {
-        $("header").addClass("active");
-        $("a").addClass("active");
-        $(".logo").attr('src', './images/logo.svg');
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $("header").addClass("scrolled");
+        $(".menu").addClass("scrolled");
+        $(".logo").attr('src','./images/logo.svg');
         $(".logo").height(58);
     } else {
-        $("header").removeClass("active");
-        $("a").removeClass("active");
-        $(".logo").attr('src', './images/logo-white.svg');
-        $(".logo").height(80);
+       $("header").removeClass("scrolled");
+       $(".menu").removeClass("scrolled");
+       $(".logo").attr('src','./images/logo-white.svg');
+       $(".logo").height(80);
     }
 });
