d724ed6b050d176d5c67cda783bf5be3bd1209d6
Vinicius Marson <vinicius.marson@singu.com.br>
2021-03-05 16:00:38 -0300

    Refactored code

--- Unidade 4/projeto/header.js
+++ Unidade 4/projeto/header.js
@@ -1,15 +1,13 @@
-$(window).on("scroll", function() {
-    if($(window).scrollTop() > 50) {
-        $("header").height(90);
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
         $("header").addClass("active");
         $("a").addClass("active");
-        $(".logo").attr('src','./images/logo.svg');
+        $(".logo").attr('src', './images/logo.svg');
         $(".logo").height(58);
     } else {
-       $("header").height(140);
-       $("header").removeClass("active");
-       $("a").removeClass("active");
-       $(".logo").attr('src','./images/logo-white.svg');
-       $(".logo").height(80);
+        $("header").removeClass("active");
+        $("a").removeClass("active");
+        $(".logo").attr('src', './images/logo-white.svg');
+        $(".logo").height(80);
     }
 });
